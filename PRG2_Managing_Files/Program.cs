b49677dcd89be9a6e593a83ab8834f36077a5679
TeacherArea <sewilius.berg@lbs.se>
2024-01-24 15:02:45 +0100

    Added a welcome text for master, where the examples are now deleted and instead points to the branches

--- PRG2_Managing_Files/Program.cs
+++ PRG2_Managing_Files/Program.cs
@@ -1,41 +1,16 @@
-﻿using System;
-using System.IO;
-namespace WriteReadToFile
+﻿namespace WriteReadToFile
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string userName = "Pelle Pelleson";
-            int guesses = 8;
-            Console.WriteLine("Hello World! And here is the High Scores: ");
-           
-                       SaveHighScore(userName, guesses);
-            LoadHighScores();
-        }
-        static private void SaveHighScore(string userName,
-       int guesses)
-        {
-            string filePath = "highscores.txt";
-            string scoreEntry = $"{userName} - {guesses}\n";
-
-            File.AppendAllText(filePath, scoreEntry);
-        }
-        static private void LoadHighScores()
-        {
-            string filePath = "highscores.txt";
-
-            if (File.Exists(filePath))
-            {
-                string[] scores = File.ReadAllLines(filePath);
-
-                foreach (string s in scores)
-                {
-                    Console.WriteLine(s);
-                }
-                //listBoxHighScores.Items.Clear();
-                //listBoxHighScores.Items.AddRange(scores);
-            }
+            Console.WriteLine("\tThe code in this repo compares and gives three examples of\n\t" +
+                                "how to handle save/read to/from files.\n\t\n\t" +
+                                "The repot has three branches (besides master):\n\t\n\t" +
+                                "1) with-file:\t\tan example of the library \"File\"\n\t" +
+                                "2) with-streams:\tan example of \"StreamWriter\" and \"StreamReader\"\n\t" +
+                                "3) with-json-and-oop:\tan example of JSON, and a start of organizing the project in a object orienteted way"
+                );
         }
     }
 }
